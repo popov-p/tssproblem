1a97f7f374d465a8ec529afe3d8d267bf0cfe9d5
popov-p <pashafizmat1@gmail.com>
2024-03-31 02:10:01 +0300

    data parsing

---
+++ data-processing.py
@@ -0,0 +1,100 @@
+import argparse
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+
+parser = argparse.ArgumentParser(description='Process traffic demand')
+parser.add_argument('-d', type=str, default='datasets/current', help='path to directory with .xlsx data')
+parser.add_argument('-o', type=str, default='datasets/processed', help='path to processed sorted .xlsx output')
+parser.add_argument('-p', type=str, default='datasets/plot-data', help='path to xlsx plot tables')
+parser.add_argument('-od', type=str, default='datasets/od-data', help='path to for od-data')
+args = parser.parse_args()
+
+def process_xlsx(file_path):
+    df = pd.read_excel(file_path)
+    df = df[['Volume', 'Number', 'Time']]
+    df['Time'] = pd.to_datetime(df['Time'])
+    df['Volume'] = df['Volume'].fillna(0)
+    return df
+
+def save_processed(df, filename):
+    processed_dir = args.o
+    os.makedirs(processed_dir, exist_ok=True)
+    filepath = os.path.join(processed_dir, filename)
+    df.to_excel(filepath, index=False)
+
+def save_processed_mean(ts, filename):
+    plot_dir = args.p
+    os.makedirs(plot_dir, exist_ok=True)
+    filepath = os.path.join(plot_dir, filename)
+    with open(filepath, 'w') as file:
+        for key, value in ts.items():
+            file.write(f"{key}: {value}\n")
+
+def save_for_od_data(ts, filename):
+    od_dir = args.od
+    os.makedirs(od_dir, exist_ok=True)
+    
+    stats = {'min': min(ts.values()),
+                  'max': max(ts.values()),
+                  'avg': int(sum(ts.values()) / len(ts))}
+    
+    filepath = os.path.join(od_dir, filename)
+    with open(filepath, 'w') as file:
+        for key, value in stats.items():
+            file.write(f"{key}: {value}\n")
+
+def filter(df):
+    return df[(df['Time'].dt.hour == 6) | 
+              ((df['Time'].dt.hour == 9) & ((df['Time'].dt.minute == 0) |
+              (df['Time'].dt.minute < 5)) | ((df['Time'].dt.hour > 6) & (df['Time'].dt.hour < 9)))]
+
+def sort_first_n_days(n, cur_df):
+    filtered_df = filter(cur_df)
+    sorted_filtered_df = filtered_df.sort_values(by='Time')
+    return sorted_filtered_df[sorted_filtered_df['Time'] < sorted_filtered_df['Time'].iloc[0] + pd.Timedelta(days=n)]
+
+def form_mean_stats(first_n_days_data):
+    time_stats = {}
+    grouped = first_n_days_data.groupby(first_n_days_data['Time'].dt.strftime('%H:%M'))
+    for group_name, group_data in grouped:
+        avg_volume = group_data['Volume'].sum() / len(group_data['Time'].dt.date.unique())
+        time_stats[group_name] = int(avg_volume)
+    return time_stats
+
+def extract_number(filename):
+    parts = filename.split('_') 
+    if len(parts) >= 3:
+        number_part = parts[2].split('.')[0]
+        return int(number_part) if number_part.isdigit() else None
+    return None
+
+def plot(mean_dict, plot_name):
+    plt.figure(figsize=(10, 6))
+    plt.bar(mean_dict.keys(), mean_dict.values(), color='skyblue')
+    plt.title('Average Volume per Time Interval, detector: ' + str(extract_number(plot_name)))
+    plt.xlabel('Time Interval')
+    plt.ylabel('Average Volume')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+def main():
+    for filename in os.listdir(args.d):
+        if filename.endswith('.xlsx'):
+            file_path = os.path.join(args.d, filename)
+            cur_df = process_xlsx(file_path)
+            n = 20
+            first_n_days_data = sort_first_n_days(n, cur_df=cur_df)
+
+            time_stats = form_mean_stats(first_n_days_data=first_n_days_data)
+            save_processed(first_n_days_data, 'proc_'+filename)
+            save_processed_mean(time_stats, 'mean_'+filename)
+            save_for_od_data(time_stats, 'range_'+filename)
+            print(time_stats)
+            print(first_n_days_data)
+            #plot(time_stats, filename)
+
+if __name__ == '__main__':
+    main()
